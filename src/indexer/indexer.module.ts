70ab4fe71ead9dcf34c77a43d5d012b3ec013690
Theezr <nickvanrossenberg@hotmail.com>
2024-09-02 14:42:46 +0200

    added extra service for known accounts etc

--- src/indexer/indexer.module.ts
+++ src/indexer/indexer.module.ts
@@ -6,11 +6,12 @@ import { IndexerRepoService } from './indexer-repo.service';
 import { IndexerGenesisService } from './indexer-genesis.service';
 import { AccountModule } from 'src/account/account.module';
 import { StateModule } from 'src/state/state.module';
+import { IndexerExtraService } from './indexer-extra.service';
 
 @Module({
   imports: [StateModule, NodeApiModule, EventModule, AccountModule],
   controllers: [],
-  providers: [IndexerRepoService, IndexerService, IndexerGenesisService],
+  providers: [IndexerRepoService, IndexerService, IndexerGenesisService, IndexerExtraService],
   exports: [IndexerService],
 })
 export class IndexerModule {}
