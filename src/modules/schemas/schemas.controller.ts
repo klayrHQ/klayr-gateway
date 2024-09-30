38ca3964cb04b9c0d1df77766bd1e2b9d508edb3
Theezr <nickvanrossenberg@hotmail.com>
2024-09-30 15:12:53 +0200

    :recycle: error handling

--- src/modules/schemas/schemas.controller.ts
+++ src/modules/schemas/schemas.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { NodeApi, NodeApiService } from '../node-api/node-api.service';
-import { GatewayResponse } from 'src/utils/controller-helpers';
+import { ControllerHelpers, GatewayResponse } from 'src/utils/controller-helpers';
 import { getSchemaResponse } from './dto/get-schemas-res.dto';
 
 @ApiTags('Schemas')
@@ -13,6 +13,10 @@ export class SchemasController {
   @ApiResponse(getSchemaResponse)
   async getSchemas() {
     const schemas = await this.nodeApi.invokeApi<any>(NodeApi.SYSTEM_GET_SCHEMA, {});
+
+    if (ControllerHelpers.isNodeApiError(schemas))
+      throw new HttpException(schemas.error, HttpStatus.NOT_FOUND);
+
     return new GatewayResponse(schemas, {});
   }
 }
