6a471c85ddeb83da58a41fc0068c416daa7ad560
Theezr <nickvanrossenberg@gmail.com>
2024-10-18 10:34:02 +0200

    :seedling: blockchain app statistics endpoint

--- src/modules/interoperability/interoperability.controller.ts
+++ src/modules/interoperability/interoperability.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { GatewayResponse } from 'src/utils/controller-helpers';
+import { ControllerHelpers, GatewayResponse } from 'src/utils/controller-helpers';
 import { GetAppsDto } from './dto/get-apps.dto';
 import { getAppsResponse } from './dto/get-apps-res.dto';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { NodeApi, NodeApiService } from '../node-api/node-api.service';
+import { AnnualInflation } from '../node-api/types';
+import { GetAppsStatsResDto, getAppsStatsResponse } from './dto/get-app-stats-res.dto';
+import { APP_STATUS } from './types';
 
 @ApiTags('Interoperability')
 @Controller('blockchain')
 export class InteroperabilityController {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly nodeApi: NodeApiService,
+  ) {}
 
   @Get('apps')
   @UsePipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }))
@@ -37,5 +52,55 @@ export class InteroperabilityController {
     ]);
 
     return new GatewayResponse(apps, { total, limit, offset });
+  }
+
+  @Get('apps/statistics')
+  @ApiResponse(getAppsStatsResponse)
+  async getSummary(): Promise<GatewayResponse<GetAppsStatsResDto>> {
+    const [registered, activated, terminated, totalSupply, escrowedKLY] = await Promise.all([
+      this.prisma.blockchainApp.count({ where: { status: APP_STATUS.REGISTERED } }),
+      this.prisma.blockchainApp.count({ where: { status: APP_STATUS.ACTIVATED } }),
+      this.prisma.blockchainApp.count({ where: { status: APP_STATUS.TERMINATED } }),
+      this.getTotalSupplyAmount(),
+      this.getTotalEscrowedAmount(),
+    ]);
+
+    const annualInflation = await this.nodeApi.invokeApi<AnnualInflation>(
+      NodeApi.REWARD_GET_ANNUAL_INFLATION,
+      {
+        height: Number(this.nodeApi.nodeInfo.height),
+      },
+    );
+
+    if (ControllerHelpers.isNodeApiError(annualInflation))
+      throw new HttpException(annualInflation.error, HttpStatus.NOT_FOUND);
+
+    return new GatewayResponse(
+      {
+        registered,
+        activated,
+        terminated,
+        totalSupplyKLY: totalSupply,
+        totalStakedKLY: escrowedKLY,
+        currentAnnualInflationRate: annualInflation.rate,
+      },
+      {},
+    );
+  }
+
+  private getTotalEscrowedAmount(): string {
+    const totalEscrowedAmount = this.nodeApi.tokenSummaryInfo.escrowedAmounts.escrowedAmounts
+      .map((item) => parseFloat(item.amount))
+      .reduce((acc, amount) => acc + amount, 0);
+
+    return totalEscrowedAmount.toString();
+  }
+
+  private getTotalSupplyAmount(): string {
+    const totalSupplyAmount = this.nodeApi.tokenSummaryInfo.totalSupply.totalSupply
+      .map((item) => parseFloat(item.totalSupply))
+      .reduce((acc, totalSupply) => acc + totalSupply, 0);
+
+    return totalSupplyAmount.toString();
   }
 }
